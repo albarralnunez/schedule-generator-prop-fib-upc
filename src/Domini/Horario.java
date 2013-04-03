c439fbfdc690072e86161b4107327669da02c2ea
Daniel Albarral <albarralnunez AT gmail DOT com>
2013-04-03 11:40:19 +0200

    Creacion Horario

---
+++ src/Domini/Horario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domini;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Horario {
+    private  ArrayList Dias;
+    
+    public Horario(int numDias){
+        Dias = new ArrayList(numDias);
+        for(int i = 0; i < numDias; i++) {
+            Dias.add(i, new ArrayList(10));
+            for (int j = 0; j < 10; j++) {
+                Dias.
+            }
+        }
+    }
+    
+}
