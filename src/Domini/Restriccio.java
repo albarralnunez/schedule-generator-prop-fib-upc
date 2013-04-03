35f39978c6a2739e65ec0dc8f23da68f0556872f
Daniel Albarral <albarralnunez AT gmail DOT com>
2013-04-03 13:13:59 +0200

    Inclusion clases compartidas

--- src/Domini/Restriccio.java
+++ src/Domini/Restriccio.java
@@ -2,14 +2,55 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Domini;
+package Domini
+;
 
 /**
  *
- * @author Daniel
+ * @author alex
  */
-public abstract class Restriccio {
+public class Restriccio {
     
-    
-    
+        //Atributs
+        int id;
+        boolean compleix;
+        
+        //Constructora
+	
+	/**
+        * Crea una instancia de la classe Restriccio.
+        * @param ID Es l'identificador associat a la restriccoi que acabem de crear. 
+        */
+	public Restriccio(int ID){
+		id = ID;
+	}
+        //Metodes
+        
+        /**
+        * Retorna l'identificador de la restriccio
+        */
+        public int ObtenirId(){
+            return id;
+        }
+        
+        /**
+        * S'assigna un identificador a una restriccio.
+        * @param ID Es l'identificador que volem assignar a la restriccio
+        */
+        public void AssignarId(int ID){
+            id = ID;
+        }
+        
+        /**
+        * Retorna el boolea que indica si es compleix o no la restriccio.
+        */
+        public boolean Compleix(){
+            return compleix;
+        }
+        
+        /**
+        * Funcio que cada restriccio especifica implementa per a saber si es compleix.
+        */
+        public void CompleixRes(){
+        }
 }
