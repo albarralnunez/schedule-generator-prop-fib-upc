8ae98663493287f6ff0317aade6c3a5d20d43ca9
JoanP <joanpol.farre@gmail.com>
2013-04-06 21:29:08 +0200

    Pujar clase CjtTitulaciones

---
+++ src/Domini/CjtTitulaciones.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domini;
+
+import java.util.ArrayList;
+/**
+ *
+ * @author Joan Pol
+ */
+public class CjtTitulaciones {
+    
+    private ArrayList<Titulacion> cjtTitulaciones;
+    
+    public CjtTitulaciones(){
+        cjtTitulaciones = new ArrayList<Titulacion>();
+    }
+    
+    public CjtTitulaciones(ArrayList<Titulacion> a){
+        cjtTitulaciones = new ArrayList<Titulacion>();
+        cjtTitulaciones = a;
+    }
+    
+    
+    public ArrayList<Titulacion> GetCjtTitulaciones() {
+        return cjtTitulaciones;
+    }
+    
+    public void AñadirTitulacion(Titulacion titulacion){
+        cjtTitulaciones.add(titulacion);
+    }
+    
+    public void BorrarTitulacion(Titulacion titulacion) {
+        if(!cjtTitulaciones.isEmpty()) {
+            if (! cjtTitulaciones.remove(titulacion)) {
+                throw new Error ("No exsiste la titulacion que se desea borrar");
+            }
+        }
+        else throw new Error ("No exsiste ninguna titlacion para borrar");
+    }
+    public Titulacion GetTitulacion(Titulacion titulacion){
+        int index = cjtTitulaciones.indexOf(titulacion);
+        if(index != -1) {
+            return cjtTitulaciones.get(index);
+        }
+        else throw new Error ("No exsiste la titulacion que se desea obtener");
+        
+    }   
+}
