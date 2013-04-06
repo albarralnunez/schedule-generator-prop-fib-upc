f2e53cbc4e6a7e015bca9acc78734a5d4bf10969
JoanP <joanpol.farre@gmail.com>
2013-04-06 21:24:57 +0200

    modificacio de la subclase grupolab

--- src/Domini/GrupoLab.java
+++ src/Domini/GrupoLab.java
@@ -9,16 +9,21 @@ package Domini;
  * @author Joan Pol
  */
 public class GrupoLab extends Grupo {
-    private boolean pc; //Si es necesita pc
+    private boolean material; //Si es necesita pc
     /* private boolean mati; //si ha de ser de mati
     private boolean tarda;  // '' de tarda
     private boolean mixte;*/ //si es indiferent
     
-    public GrupoLab (int numero,int capacidad,boolean pc) {
+    public GrupoLab (int numero,int capacidad,boolean material) {
         super(numero,capacidad);
-        
-        
-        
-        this.pc = pc;
+        this.material = material;
+    }
+    
+    public boolean GetMaterial(){
+        return material;
+    }
+    
+    public void SetMaterial(boolean mat){
+        this.material = material;
     }
 }
