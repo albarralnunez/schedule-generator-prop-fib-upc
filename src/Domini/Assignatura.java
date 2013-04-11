33f5053abad87bb2c7196bbf604ce38f66f829aa
JoanP <joanpol.farre@gmail.com>
2013-04-12 00:25:21 +0200

    Canvis array grupos de assig

--- src/Domini/Assignatura.java
+++ src/Domini/Assignatura.java
@@ -4,6 +4,9 @@
  */
 package Domini;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author miquel.masriera
@@ -13,6 +16,9 @@ public class Assignatura {
     private int horesT; // hores de teoria
     private int horesP; // hores de practica 
     private int nivel; 
+    private int capacitatTeo;
+    private int capacitatLab;
+    private ArrayList<Integer> grupos;
 
     
     public Assignatura() {
@@ -20,13 +26,18 @@ public class Assignatura {
         horesT = -1;
         horesP = -1;
         nivel = -1;
+        grupos = new ArrayList<Integer>();
     }
 
-    public Assignatura( String nm, int ht, int hp, int nv) {
+    public Assignatura( String nm, int ht, int hp, int nv,int capTeo, int capLab,ArrayList<Integer> a) {
         nom = nm;
         horesT = ht;
         horesP = hp;
         nivel = nv;
+        capacitatTeo= capTeo;
+        capacitatLab = capLab;
+        grupos = a;
+        
     }
     
     /**
@@ -63,5 +74,31 @@ public class Assignatura {
 
     public void setNivel(int nivel) {
         this.nivel = nivel;
+    }    
+    public int getCapacitatTeo() {
+        return capacitatTeo;
+    }
+
+    public void setCapacitatTeo(int capt) {
+        capacitatTeo = capt;
+    } 
+    public int getCapacitatLab() {
+        return capacitatLab;
+    }
+
+    public void setCapacitatLab(int capl) {
+        capacitatLab = capl;
+    }
+    public ArrayList<Integer> getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(ArrayList<Integer> a) {
+        grupos.addAll(a);
+        Collections.sort(grupos);
+    }
+    public void setGrupo(int grupo) {
+        grupos.add(grupo);
+        Collections.sort(grupos);
     }    
 }
