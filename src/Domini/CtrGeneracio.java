2788f22b6749e8376e5d506a771c94c4d518a976
miquel masriera quevedo <miquelmasriera@gmail.com>
2013-04-20 17:52:40 +0200

    ctr generacio

---
+++ src/Domini/CtrGeneracio.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domini;
+
+/**
+ *
+ * @author miquel
+ */
+public class CtrGeneracio {
+    
+    private RestriccioTemps resT;
+    private CjtAules cjtAu;
+    private CjtAssignatures cjtAs;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
