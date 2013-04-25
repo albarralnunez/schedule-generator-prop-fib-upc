9afe688be69b5ffc072a30f68487e80e70d2909d
Daniel Albarral <albarralnunez AT gmail DOT com>
2013-04-25 21:48:22 +0200

    Inicializacion Dominio

--- src/Domini/Generador.java
+++ src/Domini/Generador.java
@@ -11,7 +11,12 @@ import java.util.ArrayList;
  * @author Daniel Albarral
  */
 class Generador {
-    private Horari horari = new Horari();
+    
+    private Horari  h;
+
+    public Generador() {
+        this.h = null;
+    }
     
     private ArrayList<Clausula> inicialitzarClausules(CjtAules aulesT, CjtAules 
             aulesL, CjtAssignatures ass, RestriccioTemps dis) {
@@ -25,51 +30,86 @@ class Generador {
                 String dia;
                 Integer hora;
                 //Inicialitzacio clausules amb grups de laboratori         
-                if(g%10 != 0) {
-                    CjtAules aulesPos = aulesL.cjtCapacitatMajorDe(g);
-                    int i = 0;
-                    for (Integer h : a.getIntervalsP()) {
-                        c.setDuracio(h);
-                        clausules.add(c);
-                        //Inicialitzacio del domini
-                        for (Aula au : aulesPos.getCjtAules()){
-                            nomAul = au.getNom();
-                            for (Pair d : dis.disponibilitat()){
-                                dia = (String)d.getL();
-                                hora = (Integer)d.getR();
-                                clausules.get(i).afegirElem(nomAul, dia, hora);
-                            }
-                        }
-                        ++i;
-                    }
+                CjtAules aulesPos = new CjtAules();
+                ArrayList<Integer> interval = new ArrayList<Integer>();
+                if(g%10 != 0){  //GupsLab     
+                     aulesPos = aulesL.cjtCapacitatMajorDe(a.getCapacitatLab());
+                     interval = a.getIntervalsP();
                 }
-                else {
-                    CjtAules aulesPos = aulesT.cjtCapacitatMajorDe(a.getCapacitatTeo());
-                    int i = 0;
-                    for (Integer h : a.getIntervalsT()) {
-                        c.setDuracio(h);
-                        clausules.add(c);
-                        //Inicialitzacio del domini
-                        for (Aula au : aulesPos.getCjtAules()){
-                            nomAul = au.getNom();
-                            for (Pair d : dis.disponibilitat()){
-                                dia = (String)d.getL();
-                                hora = (Integer)d.getR();
-                                clausules.get(i).afegirElem(nomAul, dia, hora);
-                            }
-                        }
-                        ++i;
-                    }
+                else{ //GrupsTeo
+                    aulesPos = aulesT.cjtCapacitatMajorDe(a.getCapacitatTeo());
+                    interval = a.getIntervalsT();
                 }
+                for (Integer h : interval) {
+                    c.setDuracio(h);
+                    //Inicialitzacio del domini
+
+                    for (Aula au : aulesPos.getCjtAules()){
+                        nomAul = au.getNom();
+                        for (int j = 0; j < 7; ++j) {
+                            if (j == 0) {
+                                 dia = "dilluns";
+                               for (Integer d : dis.getDilluns()){
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 1) {
+                                dia = "dimarts";
+                               for (Integer d : dis.getDimarts()){                                     
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 2) {
+                                dia = "dimecres";
+                               for (Integer d : dis.getDimecres()){
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 3) {
+                                dia = "dijous";
+                                for (Integer d : dis.getDijous()){
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 4) {
+                                dia = "divendres";
+                                for (Integer d : dis.getDivendres()){
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 5) {
+                                dia = "dissabte";
+                                for (Integer d : dis.getDissabte()){
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }
+                            if (j == 6) {
+                                dia = "diumenge";
+                                for (Integer d : dis.getDiumenge()){     
+                                    hora = d;
+                                    c.afegirElem(nomAul, dia, hora);
+                               }
+                            }       
+                        }
+                    }
+                    clausules.add(c);
+                }  
             }
-        }
+        }          
         return clausules;
     }
             
     public Horari generar(CjtAules aulesT, CjtAules aulesL, CjtAssignatures ass,
         RestriccioTemps dis) {
+        h = new Horari();
         ArrayList<Clausula> clau = inicialitzarClausules(aulesT,aulesL,ass,dis);
-        return horari;
+        return h;
     }
     
 }
