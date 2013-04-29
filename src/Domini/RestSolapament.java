18ab3c43d32ee04868ba3448939af4b513e9bb2d
JoanP <joanpol.farre@gmail.com>
2013-04-29 02:06:26 +0200

    funcions compleiRES

--- src/Domini/RestSolapament.java
+++ src/Domini/RestSolapament.java
@@ -75,12 +75,104 @@ public class RestSolapament extends Restriccio{
         return true;
     }
     
-    public boolean CompleixRes(String assignaturaP, int grupP, String assignaturaS,int grupoS) {
-       if(this.assignaturaPrincipal == assignaturaP && this.grupPrincipal == grupP){
-           if(this.assignaturaSolapament == assignaturaS && this.grupSolapament != grupoS) return false;
-         else if(this.assignaturaSolapament != assignaturaS) return false;
-       }
-       return true;
+    private String transformaIntDia(int i){
+        if (i == 0) return "dilluns"; 
+        else if (i == 1) return "dimarts";
+        else if (i == 2) return "dimecres";
+        else if (i == 3) return "dijous";
+        else if (i == 4) return "divendres";
+        else if (i == 5) return "dissabte";
+        else return "diumenge";
     }
-   
+    public boolean CompleixRes(Quadricula q, Element e, String dia,int hora) {
+       boolean ass1 = false;
+       boolean ass2 = false;
+       boolean grup1 = false;
+       boolean grup2 = false;
+       CjtElements cjt = new CjtElements();
+       cjt = q.getElementsPosicio(dia, hora);
+       int size = cjt.numeroElements();
+       if(e.getAssignatura().equals(assignaturaPrincipal)){
+           ass1 = true;
+           if(grupPrincipal >= 0){
+               if(grupPrincipal != e.getGrupo()) ass1 = false;
+               else grup1 = true;
+           }
+       }
+       else if(e.getAssignatura().equals(assignaturaSolapament)){
+            ass2 = true;
+            if(grupSolapament >= 0){
+               if(grupSolapament != e.getGrupo()) ass2 = false;
+               else grup2 = true;
+           }
+       }
+       if (ass1 == false && ass2 == false) return true;
+       for(int i = 0; i < size; ++i){
+           if(ass1 != false){
+               if(cjt.getElementPosicio(i).getAssignatura().equals(assignaturaSolapament)) {
+                    ass2 = true;
+                    if(cjt.getElementPosicio(i).getGrupo() == grupSolapament) grup2 = true;
+                }
+           }
+           else if(ass2 != false){
+                if(cjt.getElementPosicio(i).getAssignatura().equals(assignaturaPrincipal)) {
+                    ass1 = true;
+                    if(cjt.getElementPosicio(i).getGrupo() == grupPrincipal) grup1 = true;
+                }   
+           }
+            if(grupPrincipal <0 && grupSolapament < 0) {
+                if(ass1 && ass2) return false;
+            }
+            else if(grupPrincipal <0 && grupSolapament >= 0){
+                if(ass1 && ass2 && grup2) return false;
+            }
+            else if(grupPrincipal >=0 && grupSolapament < 0){
+                if(ass1 && ass2 && grup1) return false;
+            }
+            else {
+                if(ass1 && ass2 && grup1 && grup2) return false;
+            }
+        }
+        return true;
+   }
+   public boolean CompleixRes(Quadricula q){
+       boolean ass1;
+       boolean ass2;
+       boolean grup1;
+       boolean grup2;
+       
+       CjtElements cjt = new CjtElements();
+       for(int i=0; i < 7; ++i) {
+            for (int j=0; j < 24; ++j) { 
+                ass1 = false;
+                ass2 = false;
+                grup1 = false;
+                grup2 = false;
+                cjt = q.getElementsPosicio(transformaIntDia(i), j);
+                for(int k = 0; k < cjt.numeroElements(); ++k){
+                    if(cjt.getElementPosicio(k).getAssignatura().equals(assignaturaPrincipal)) {
+                        ass1 = true;
+                        if(cjt.getElementPosicio(k).getGrupo() == grupPrincipal) grup1 = true;
+                    }
+                    if(cjt.getElementPosicio(k).getAssignatura().equals(assignaturaSolapament)) {
+                         ass2 = true;
+                        if(cjt.getElementPosicio(k).getGrupo() == grupSolapament) grup2 = true;
+                    }
+                    if(grupPrincipal <0 && grupSolapament < 0) {
+                        if(ass1 && ass2) return false;
+                    }
+                    else if(grupPrincipal <0 && grupSolapament >= 0){
+                        if(ass1 && ass2 && grup2) return false;
+                    }
+                    else if(grupPrincipal >=0 && grupSolapament < 0){
+                        if(ass1 && ass2 && grup1) return false;
+                    }
+                    else {
+                        if(ass1 && ass2 && grup1 && grup2) return false;
+                    }
+               }
+            }
+      }
+      return true;
+    }
 }
