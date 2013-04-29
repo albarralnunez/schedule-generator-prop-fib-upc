f67b2d2582187f779c346169009f18a3e9b8a890
JoanP <joanpol.farre@gmail.com>
2013-04-29 14:10:07 +0200

    arreglillos

--- src/Domini/RestSolapament.java
+++ src/Domini/RestSolapament.java
@@ -11,52 +11,61 @@ import java.util.ArrayList;
  * @author Joan Pol
  */
 public class RestSolapament extends Restriccio{
-    String assignaturaPrincipal;
+    Assignatura assignaturaPrincipal;
     int grupPrincipal;
-    String assignaturaSolapament;
+    Assignatura assignaturaSolapament;
     int grupSolapament;
     
     /** 
     * Crea una restriccion que indica que una assig+grupo no puede impartirse a la vez que otra temporalmente hablando
     */
     public RestSolapament(){
-        super(0);
+        super(4);
         assignaturaPrincipal = null;
         grupPrincipal = -1;
         assignaturaSolapament = null;
         grupSolapament = -1;
     }
-    public RestSolapament(String assignaturaP, int grupP, String assignaturaS, int grupS){
+    public RestSolapament(Assignatura assignaturaP, int grupP, Assignatura assignaturaS, int grupS){
         super(0);
         this.assignaturaPrincipal = assignaturaP;
         this.grupPrincipal = grupP;
         this.assignaturaSolapament = assignaturaS;
         this.grupSolapament = grupS;
     }
-    public void setAssignaturaPrincipal(String assignaturaP){
-        this.assignaturaPrincipal = assignaturaP;
-    }
-    public void setAssignaturaSolapamiento(String assignaturaS){
-        this.assignaturaSolapament = assignaturaS;
-    }
-    public void setGrupPrincipal(int grupP){
-        this.grupPrincipal =grupP;
-    }
-    public void setGrupSolapament(int grupS){
-        this.grupSolapament = grupS;
-    }
-    public String getAssignaturaPrincipal(){
+
+    public Assignatura getAssignaturaPrincipal() {
         return assignaturaPrincipal;
     }
-    public String getAssignaturaSolapamiento(){
-        return assignaturaSolapament;
-    }
-    public int getGrupPrincipal(){
+
+    public int getGrupPrincipal() {
         return grupPrincipal;
     }
-    public int getGrupSolapament(){
+
+    public Assignatura getAssignaturaSolapament() {
+        return assignaturaSolapament;
+    }
+
+    public int getGrupSolapament() {
         return grupSolapament;
     }
+
+    public void setAssignaturaPrincipal(Assignatura assignaturaPrincipal) {
+        this.assignaturaPrincipal = assignaturaPrincipal;
+    }
+
+    public void setGrupPrincipal(int grupPrincipal) {
+        this.grupPrincipal = grupPrincipal;
+    }
+
+    public void setAssignaturaSolapament(Assignatura assignaturaSolapament) {
+        this.assignaturaSolapament = assignaturaSolapament;
+    }
+
+    public void setGrupSolapament(int grupSolapament) {
+        this.grupSolapament = grupSolapament;
+    }
+    
     
     @Override
     public boolean CompleixRes(){return false;}
