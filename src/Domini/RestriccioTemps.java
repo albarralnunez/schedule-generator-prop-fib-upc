5d364681f70a14846cdca71dea8346fb7baf66c5
miquel masriera quevedo <miquelmasriera@gmail.com>
2013-05-02 03:16:32 +0200

    fsqdafsfs

--- src/Domini/RestriccioTemps.java
+++ src/Domini/RestriccioTemps.java
@@ -21,63 +21,108 @@ public class RestriccioTemps {
     private ArrayList<Integer> diumenge;
 
 
-    //Constructora por defecto
+    /**
+     * 
+     */
     public RestriccioTemps() {}
 
-
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDissabte() {
         return dissabte;
     }
-
+    /**
+     * 
+     * @param dissabte 
+     */
     public void setDissabte(ArrayList<Integer> dissabte) {
         this.dissabte = dissabte;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDiumenge() {
         return diumenge;
     }
-
+    /**
+     * 
+     * @param diumenge 
+     */
     public void setDiumenge(ArrayList<Integer> diumenge) {
         this.diumenge = diumenge;
     }
     
-    
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDilluns() {
         return dilluns;
     }
 
+    /**
+     * 
+     * @param dilluns 
+     */
     public void setDilluns(ArrayList<Integer> dilluns) {
         this.dilluns = dilluns;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDimarts() {
         return dimarts;
     }
-
+    /**
+     * 
+     * @param dimarts 
+     */
     public void setDimarts(ArrayList<Integer> dimarts) {
         this.dimarts = dimarts;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDimecres() {
         return dimecres;
     }
-
+    /**
+     * 
+     * @param dimecres 
+     */
     public void setDimecres(ArrayList<Integer> dimecres) {
         this.dimecres = dimecres;
     }
-
+    /**
+     * 
+     * @return 
+     */
     public ArrayList<Integer> getDijous() {
         return dijous;
     }
-
+    /**
+     * 
+     * @param dijous 
+     */
     public void setDijous(ArrayList<Integer> dijous) {
         this.dijous = dijous;
     }
-
+/**
+ * 
+ * @return 
+ */
     public ArrayList<Integer> getDivendres() {
         return divendres;
     }
-
+    /**
+     * 
+     * @param divendres 
+     */
     public void setDivendres(ArrayList<Integer> divendres) {
         this.divendres = divendres;
     }
